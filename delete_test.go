ed22449e7a257a14cd583ba7404fc7a35aa408a1
yuchanns <me@yuchanns.xyz>
2024-07-10 15:34:29 +0800

    Add Delete Tests
    Signed-off-by: Hanchin Hsieh <me@yuchanns.xyz>

--- delete_test.go
+++ delete_test.go
@@ -3,6 +3,7 @@ package opendal_test
 import (
 	"opendal"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,46 +16,45 @@ func testsDelete(cap *opendal.Capability) []behaviorTest {
 		testDeleteEmptyDir,
 		testDeleteWithSpecialChars,
 		testDeleteNotExisting,
-		testDeleteStream,
-		testRemoveOneFile,
-	}
-	if cap.ListWithRecursive() {
-		tests = append(tests, testRemoveAllBasic)
-		if !cap.CreateDir() {
-			tests = append(tests, testRemoveAllWithPrefix)
-		}
 	}
 	return tests
 }
 
 func testDeleteFile(assert *require.Assertions, op *opendal.Operator, fixture *fixture) {
-	//
+	path, content, _ := fixture.NewFile()
+
+	assert.Nil(op.Write(path, content), "write must succeed")
+
+	assert.Nil(op.Delete(path))
+
+	assert.False(op.IsExist(path))
 }
 
 func testDeleteEmptyDir(assert *require.Assertions, op *opendal.Operator, fixture *fixture) {
-	//
+	if !op.Info().GetFullCapability().CreateDir() {
+		return
+	}
+
+	path := fixture.NewDirPath()
+
+	assert.Nil(op.CreateDir(path), "create must succeed")
+
+	assert.Nil(op.Delete(path))
 }
 
 func testDeleteWithSpecialChars(assert *require.Assertions, op *opendal.Operator, fixture *fixture) {
-	//
+	path := uuid.NewString() + " !@#$%^&()_+-=;',.txt"
+	path, content, _ := fixture.NewFileWithPath(path)
+
+	assert.Nil(op.Write(path, content), "write must succeed")
+
+	assert.Nil(op.Delete(path))
+
+	assert.False(op.IsExist(path))
 }
 
 func testDeleteNotExisting(assert *require.Assertions, op *opendal.Operator, fixture *fixture) {
-	//
-}
+	path := uuid.NewString()
 
-func testDeleteStream(assert *require.Assertions, op *opendal.Operator, fixture *fixture) {
-	//
-}
-
-func testRemoveOneFile(assert *require.Assertions, op *opendal.Operator, fixture *fixture) {
-	//
-}
-
-func testRemoveAllBasic(assert *require.Assertions, op *opendal.Operator, fixture *fixture) {
-	//
-}
-
-func testRemoveAllWithPrefix(assert *require.Assertions, op *opendal.Operator, fixture *fixture) {
-	//
+	assert.Nil(op.Delete(path))
 }
